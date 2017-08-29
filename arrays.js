fd40d6ad7edfaf4aec9d2044e9cb429de038ea87
Cayla Schwartz <cayschwartz@gmail.com>
2017-08-29 19:53:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,3 +6,11 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   return array.unshift(element)
 
 }
+
+function addElementToEndOfArray(array, element){
+    return [...array, element]
+}
+function destructivelyAddElementToEndOfArray(array, element){
+  return array.push(element)
+
+}
